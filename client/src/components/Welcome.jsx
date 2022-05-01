7735baa6a312b32fd923c767d640f276140fbc83
Andrea Ferrario <andreaantonio.ferrario@mail.polimi.it>
2022-05-01 19:29:27 +0200

    welcome component

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,130 @@
+import React from "react";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { Loader } from "./";
+
+const commonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  ></input>
+);
+
+const Welcome = () => {
+  const connectWallet = () => {};
+
+  const handleSubmit = () => {};
+  return (
+    <div className="flex w-full justify-center items-center">
+      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-strat flex-col md:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1 ">
+            Invia Crypto <br /> across the world
+          </h1>
+          <p className="text-left mt-5 text-white font-light md:w-9/12 w- 11/12 text-base ">
+            Explore the cryto world. BUy and sell cryptocurrencies easily on
+            Krypto.
+          </p>
+          <button
+            type="button"
+            className="flex flex-row justify-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+            onClick={connectWallet}
+          >
+            <p className="text-white text-base font-semibold">
+              Connetti il wallet
+            </p>
+          </button>
+
+          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
+            <div className={` ${commonStyles}`}>Security</div>
+            <div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
+            <div className={` ${commonStyles}`}>Low fees</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+          </div>
+        </div>
+
+        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10 ">
+          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorpism">
+            <div className="flex justify-between flex-col w-full h-full ">
+              <div className="flex justify-between items-start ">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 320 512"
+                    className="fill-white w-6 h-6"
+                  >
+                    <path d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z" />
+                  </svg>
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  className="fill-white w-4 h-4"
+                >
+                  <path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 128c17.67 0 32 14.33 32 32c0 17.67-14.33 32-32 32S224 177.7 224 160C224 142.3 238.3 128 256 128zM296 384h-80C202.8 384 192 373.3 192 360s10.75-24 24-24h16v-64H224c-13.25 0-24-10.75-24-24S210.8 224 224 224h32c13.25 0 24 10.75 24 24v88h16c13.25 0 24 10.75 24 24S309.3 384 296 384z" />
+                </svg>
+              </div>
+              <div>
+                <p className="text-white font-light text-semibold">Address</p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-5 sm:w-96 w-full flex flex-col  justify-start items-center blue-glassmorphism">
+            <Input
+              placeholder="Address To"
+              name="addressTo"
+              type="text"
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder="Amount (ETH)"
+              name="amount"
+              type="number"
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder="Keyword (Gif)"
+              name="keyword"
+              type="text"
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder="Enter Message"
+              name="message"
+              type="text"
+              handleChange={() => {}}
+            />
+
+            <div className="h-[1px] w-full bg-gray-400 my-2"></div>
+
+            {false ? (
+              <Loader />
+            ) : (
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+              >
+                Invia Ora
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
